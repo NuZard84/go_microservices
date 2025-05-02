f99d35a02f33e6b67af282916ac44de61509d8c4
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2025-05-02 12:46:56 +0530

    resolvers are setted

---
+++ graphql/mutation_resolver.go
@@ -0,0 +1,17 @@
+package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (mr *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
+
+// }
+
+// func (mr *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+
+// }
+
+// func (mr *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+
+// }
